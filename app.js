e2d202583431948c91adc80bc7aafaf0ee935b30
F-loat <945852046@qq.com>
2017-05-16 13:55:18 +0800

    add upload base path exists judgement

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ mongoose.connect(config.get('mongodb'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/request', require('./routes/'));
+app.use('/request', require('./routes'));
 
 module.exports = app;
--- utils/upload.js
+++ utils/upload.js
@@ -1,18 +1,25 @@
 const fs = require('fs');
+const path = require('path');
 const config = require('config');
 const multer  = require('multer');
 
 const uploadPath = config.get('uploadPath');
+const episodePath = path.join(uploadPath, 'episode');
+const posterPath = path.join(uploadPath, 'poster');
+if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath);
+if (!fs.existsSync(episodePath)) fs.mkdirSync(episodePath);
+if (!fs.existsSync(posterPath)) fs.mkdirSync(posterPath);
+
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     const date = new Date();
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
-    const basePath = `${uploadPath}/${file.fieldname}`;
-    const yearPath = `${basePath}/${year}`;
-    const monthPath = `${basePath}/${year}/${month}`;
-    const dayPath = `${basePath}/${year}/${month}/${day}`;
+    const basePath = path.join(uploadPath, file.fieldname);
+    const yearPath = path.join(basePath, String(year));
+    const monthPath = path.join(yearPath, String(month));
+    const dayPath = path.join(monthPath, String(day));
     if (!fs.existsSync(yearPath)) fs.mkdirSync(yearPath);
     if (!fs.existsSync(monthPath)) fs.mkdirSync(monthPath);
     if (!fs.existsSync(dayPath)) fs.mkdirSync(dayPath);
