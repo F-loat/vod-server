f52f4d017d847f684e6f26e749282d7d9aea50e7
F-loat <945852046@qq.com>
2017-05-25 21:46:23 +0800

    fix pm2 log pah

--- pm2.config.js
+++ pm2.config.js
@@ -4,8 +4,8 @@ module.exports = {
     script: './bin/www',
     watch: false, // 默认关闭watch 可替换为 ['src']
     ignore_watch: ['node_modules', 'logs'],
-    out_file: '/logs/pm2-out.log', // 日志输出
-    error_file: '/logs/pm2-error.log', // 错误日志
+    out_file: './logs/pm2-out.log', // 日志输出
+    error_file: './logs/pm2-error.log', // 错误日志
     max_memory_restart: '800M', // 超过多大内存自动重启，仅防止内存泄露有意义，需要根据自己的业务设置
     env: {
       NODE_ENV: 'production',
