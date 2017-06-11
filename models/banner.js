50c4e63bb88958332e956c010649c7ccd46068f3
F-loat <945852046@qq.com>
2017-06-11 11:00:06 +0800

    banner controller base

--- models/banner.js
+++ models/banner.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
+const defaultDeadtime = 7 * 24 * 60 * 60 * 1000;
 
 /**
  * @param title         {String}    标题
+ * @param summary       {String}    描述
  * @param type          {String}    类型
  * @param href          {String}    链接
  * @param filePath      {String}    文件位置
@@ -18,15 +20,22 @@ const ObjectId = Schema.Types.ObjectId;
 
 const bannerSchema = new Schema({
   title: { type: String },
-  type: { type: String },
+  summary: { type: String },
+  type: { type: Number },
   href: { type: String },
   filePath: { type: String },
   deleted: { type: Boolean, default: false },
   creater: { type: ObjectId, ref: 'User' },
-  deadline: { type: Date },
+  deadline: { type: Date, default: Date.now() + defaultDeadtime },
   sort: { type: Number, default: 0, index: true },
 }, {
   timestamps: true,
+});
+
+bannerSchema.pre('save', function replace(next) {
+  const filePath = this.filePath;
+  this.filePath = filePath && filePath.replace(/\\/g, '/');
+  next();
 });
 
 const Banner = mongoose.model('Banner', bannerSchema);
