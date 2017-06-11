35d3e98c2f48553c7b8c5ce63dc18501f7f4320d
F-loat <945852046@qq.com>
2017-06-11 11:01:11 +0800

    fix log controller

--- controllers/log.js
+++ controllers/log.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 
 exports.list = async (ctx) => {
-  const log = fs.readFileSync('./logs/pm2-out-0.log', {
-    encoding: 'utf8',
-  });
+  const logPath = './logs/pm2-out-0.log';
+  if (!fs.existsSync(logPath)) {
+    ctx.body = { state: 1, content: 'log file not exist' };
+    return;
+  }
+  const log = fs.readFileSync(logPath, { encoding: 'utf8' });
   ctx.body = { state: 1, content: log };
 };
